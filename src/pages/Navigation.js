577e2800bbe8e08b15df9a309caf75eaf139c03c
samuel619zevo <sam.kush619@gmail.com>
2021-11-11 15:16:16 +0530

    dropdown refined in navbar in third page

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import NavData from '../Data/NavData.json'
+import NavData from '../data/NavData.json'
 import Coin from '../assets/Imagejs/Coin';
 import Dewallstreet from '../assets/Imagejs/Dewallstreet';
 import Map from '../assets/Imagejs/Map';
 import Navdown from '../assets/Imagejs/Navdown';
+import {useState} from 'react';
 
 function Navigation() {
+
+  const [state, setState] = useState({ clicked: false, name: "option" });
+  function clickHandler() {
+    setState({
+      clicked: !state.clicked,
+      name: state.clicked ? "option" : "optionclicked",
+    });
+  }
 
     const renderData = NavData.map((Data) => (
         <b>
@@ -20,18 +29,39 @@ function Navigation() {
     <div className='Menu_bg'>
         <div className="Logonav" >
               
-              <Link to ="/Navigation" ><Coin/></Link>
+              <Link to ="/" ><Coin/></Link>
               <div>
-                <div className="dewallstreettxt"><Dewallstreet/></div>
+                <div className="dewallstreettxt"><Link to ="/Navigation" ><Dewallstreet/></Link></div>
                 <div className="exchangestxt">exchanges.</div>
               </div>
         </div>
-        <div className="Navmap">
+        {/* <div className="Navmap">
             <div><Map/></div>
             <div className="universaltxt"><span>Universal</span></div>
             <div className="navdown"><Navdown/></div>
+        </div> */}
+        <div>
+        <div className="Navmap" onClick={clickHandler}>
+        <div><Map/></div>
+            <div className="universaltxt"><span>Universal</span></div>
+            <div className="navdown"><Navdown/></div>
         </div>
-        <div style={{marginLeft:"30px"}}>
+        <div className="dropdownDown">
+          <div className={state.name} onClick={clickHandler}>
+            Universal
+          </div>
+          <div className={state.name} onClick={clickHandler}>
+            India
+          </div>
+          <div className={state.name} onClick={clickHandler}>
+            Somalia
+          </div>
+          <div className={state.name} onClick={clickHandler}>
+            Iraq
+          </div>
+        </div>
+      </div>
+        <div style={{marginLeft:"-10px"}}>
           <li className='Menu_contents'>
             {renderData}
           </li>
