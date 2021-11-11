59b3929f5483b93e558c6c3c506033a719703be8
ArunKutty798 <arunkutty798@gmail.com>
2021-11-11 15:51:48 +0530

    changes

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import NavData from '../data/NavData.json'
-import Coin from '../assets/Imagejs/Coin';
-import Dewallstreet from '../assets/Imagejs/Dewallstreet';
-import Map from '../assets/Imagejs/Map';
-import Navdown from '../assets/Imagejs/Navdown';
-import {useState} from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import NavData from "../data/navData.json";
+import Coin from "../assets/Imagejs/Coin";
+import Dewallstreet from "../assets/Imagejs/Dewallstreet";
+import Map from "../assets/Imagejs/Map";
+import Navdown from "../assets/Imagejs/Navdown";
+import { useState } from "react";
 
 function Navigation() {
-
   const [state, setState] = useState({ clicked: false, name: "option" });
   function clickHandler() {
     setState({
@@ -17,34 +16,45 @@ function Navigation() {
     });
   }
 
-    const renderData = NavData.map((Data) => (
-        <b>
-          <Link to={Data.to} className="Menu_contents">
-            {Data.title}
-          </Link>
-        </b>
-      ));
+  const renderData = NavData.map((Data) => (
+    <b>
+      <Link to={Data.to} className="Menu_contents">
+        {Data.title}
+      </Link>
+    </b>
+  ));
 
-    return (
-    <div className='Menu_bg'>
-        <div className="Logonav" >
-              
-              <Link to ="/" ><Coin/></Link>
-              <div>
-                <div className="dewallstreettxt"><Link to ="/Navigation" ><Dewallstreet/></Link></div>
-                <div className="exchangestxt">exchanges.</div>
-              </div>
+  return (
+    <div className="Menu_bg">
+      <div className="Logonav">
+        <Link to="/">
+          <Coin />
+        </Link>
+        <div>
+          <div className="dewallstreettxt">
+            <Link to="/Navigation">
+              <Dewallstreet />
+            </Link>
+          </div>
+          <div className="exchangestxt">exchanges.</div>
         </div>
-        {/* <div className="Navmap">
+      </div>
+      {/* <div className="Navmap">
             <div><Map/></div>
             <div className="universaltxt"><span>Universal</span></div>
             <div className="navdown"><Navdown/></div>
         </div> */}
-        <div>
+      <div>
         <div className="Navmap" onClick={clickHandler}>
-        <div><Map/></div>
-            <div className="universaltxt"><span>Universal</span></div>
-            <div className="navdown"><Navdown/></div>
+          <div>
+            <Map />
+          </div>
+          <div className="universaltxt">
+            <span>Universal</span>
+          </div>
+          <div className="navdown">
+            <Navdown />
+          </div>
         </div>
         <div className="dropdownDown">
           <div className={state.name} onClick={clickHandler}>
@@ -61,14 +71,11 @@ function Navigation() {
           </div>
         </div>
       </div>
-        <div style={{marginLeft:"-10px"}}>
-          <li className='Menu_contents'>
-            {renderData}
-          </li>
-        </div>
+      <div style={{ marginLeft: "-10px" }}>
+        <li className="Menu_contents">{renderData}</li>
+      </div>
     </div>
-
-    )
+  );
 }
 
-export default Navigation
+export default Navigation;
