74dc8d7c881b936cf8045cf697005f9091feee89
Arunram07 <arunram@zevocorp.in>
2021-11-19 11:59:46 +0530

    choke : text components styles

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../styles/components/Text.module.css";
 
 const Text = (props) => {
   const {
@@ -13,14 +14,14 @@ const Text = (props) => {
     textShadow,
     fontFamily,
     letterSpacing,
-    cursor
+    cursor,
   } = props;
 
   const primaryStyles = {
     fontFamily: "Staatliches",
     fontSize: fontSize || "59.7px",
-    textShadow:  textShadow||"0px 5.52942px 5.52942px rgba(0, 0, 0, 0.25)",
-    letterSpacing: letterSpacing ||  "0.4px",
+    textShadow: textShadow || "0px 5.52942px 5.52942px rgba(0, 0, 0, 0.25)",
+    letterSpacing: letterSpacing || "0.4px",
     color: "#ffffff",
     ...style,
   };
@@ -31,13 +32,13 @@ const Text = (props) => {
     letterSpacing: letterSpacing || "-0.04em",
     color: "#ffffff",
     textShadow: textShadow || "0px 7.50275px 7.50275px rgba(0, 0, 0, 0.25)",
-    cursor:cursor,
-    className:className,
+    cursor: cursor,
+    className: className,
     ...style,
   };
 
   const errorStyles = {
-    fontFamily:  fontFamily ||"Gilroy-Medium",
+    fontFamily: fontFamily || "Gilroy-Medium",
     fontSize: fontSize || "86.9px",
     lineHeight: lineHeight || "18px",
     letterSpacing: "-0.04em",
@@ -45,46 +46,49 @@ const Text = (props) => {
     ...style,
   };
 
-
   const defaultStyles = {
     fontFamily: "Gilroy-Medium",
     fontSize: fontSize || "14px",
     lineHeight: lineHeight || "18px",
     letterSpacing: "0.03em",
     color: "#ffffff",
-    textShadow: textShadow||"0px 4px 4px rgba(0, 0, 0, 0.25)",
+    textShadow: textShadow || "0px 4px 4px rgba(0, 0, 0, 0.25)",
     ...style,
   };
 
-  const styles = () => {
-    if (variant === "primary") {
-      return primaryStyles;
+  const getClassName = () => {
+    switch (variant) {
+      case "primary":
+        return styles.primaryStyles;
+      case "secondary":
+        return styles.primaryStyles;
+      case "error":
+        return styles.error;
+      case "success":
+        return styles.success;
+      case "warning":
+        return styles.warning;
+      default:
+        return defaultStyles;
     }
-    if (variant === "secondary") {
-      return secondaryStyles;
-    }
-    if(variant === "error"){
-        return errorStyles
-    }
-    return defaultStyles;
   };
 
   switch (component) {
     case "text":
       return (
-        <text style={styles()} onClick={onClick}>
+        <text className={getClassName()} onClick={onClick}>
           {children}
         </text>
       );
     case "div":
       return (
-        <div style={styles()} onClick={onClick}>
+        <div className={getClassName()} onClick={onClick}>
           {children}
         </div>
       );
     default:
       return (
-        <p style={styles()} onClick={onClick}>
+        <p className={getClassName()} onClick={onClick}>
           {children}
         </p>
       );
